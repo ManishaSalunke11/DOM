edf74ecf45dae6e440fb4fd99490ce5a046ca6f8
ManishaSalunke <msalunke928@gmail.com>
2023-06-14 13:31:00 +0530

    Update learningJson.js

--- learningJson.js
+++ learningJson.js
@@ -14,3 +14,14 @@ const msdhoni = JSON.parse(jsonStr);
 console.log(typeof jsonStr);
 console.log(typeof msdhoni);
 console.log(msdhoni);
+console.log(msdhoni.hobbies);
+console.log(msdhoni.age);
+
+const college = {
+    name: "COEP",
+    year: 1990,
+    city: "Pune"
+}
+const str = JSON.stringify(college);
+console.log(typeof str);
+console.log(str);
