40ee7fdb036533ed10ba6a45971b77f95b2c99bc
ManishaSalunke <msalunke928@gmail.com>
2023-06-27 14:56:08 +0530

    practise

---
+++ practise.js
@@ -0,0 +1,5 @@
+let arrayIntegers = [1, 2, 3, 4, 5];
+let arrayIntegers1 = arrayIntegers.slice(0, 2); // returns [1,2]
+console.log(arrayIntegers1);
+let arrayIntegers2 = arrayIntegers.slice(2, 3); // returns [3]
+let arrayIntegers3 = arrayIntegers.slice(4); //returns [5]
